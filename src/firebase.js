8fede8b560a2b2b599ddc861e260e53a494fda60
Akr1s <dimakr71@gmail.com>
2021-07-06 14:48:39 +0300

    added firebase hosting

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+// const firebaseApp = firebase.initializeApp({
+//   apiKey: "AIzaSyDbqWZwx61URpU4AMw1e78c1oT8OO-2360",
+//   authDomain: "clone-d7f2d.firebaseapp.com",
+//   projectId: "clone-d7f2d",
+//   storageBucket: "clone-d7f2d.appspot.com",
+//   messagingSenderId: "801434470446",
+//   appId: "1:801434470446:web:16d9ad5683fe4921dcb7bf",
+// });
+
+const auth = firebase.auth();
+
+export { auth };
