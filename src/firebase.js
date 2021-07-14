8cb800fb4e76864e6f93acf1b19fde9caa285162
Akr1s <dimakr71@gmail.com>
2021-07-14 11:19:09 +0300

    added firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase";
 
-// const firebaseApp = firebase.initializeApp({
-//   apiKey: "AIzaSyDbqWZwx61URpU4AMw1e78c1oT8OO-2360",
-//   authDomain: "clone-d7f2d.firebaseapp.com",
-//   projectId: "clone-d7f2d",
-//   storageBucket: "clone-d7f2d.appspot.com",
-//   messagingSenderId: "801434470446",
-//   appId: "1:801434470446:web:16d9ad5683fe4921dcb7bf",
-// });
+firebase.initializeApp({
+  apiKey: "AIzaSyDbqWZwx61URpU4AMw1e78c1oT8OO-2360",
+  authDomain: "clone-d7f2d.firebaseapp.com",
+  projectId: "clone-d7f2d",
+  storageBucket: "clone-d7f2d.appspot.com",
+  messagingSenderId: "801434470446",
+  appId: "1:801434470446:web:16d9ad5683fe4921dcb7bf",
+});
 
 const auth = firebase.auth();
+const db = firebase.firestore();
 
-export { auth };
+export { auth, db };
